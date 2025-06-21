4bb8d818ee767370faf1ad590e6688affbf07a5f
Sebastian Jezierski <dev@sebjez.pl>
2025-06-21 05:13:01 +0200

    config 2.1.x

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1197,7 +1197,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 95 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 415 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
